a386eea0db0ad89699dbc4aee9be32940e468571
frank <frank@DESKTOP-EKQ6KKU>
2025-09-23 18:05:55 +0100

    feat: add Visual Studio compatible migrations for Mac changes

--- EasyGames/Migrations/20250923170257_MacChanges.Designer.cs
+++ EasyGames/Migrations/20250923170257_MacChanges.Designer.cs
@@ -4,6 +4,7 @@ using EasyGames.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EasyGames.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250923170257_MacChanges")]
+    partial class MacChanges
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
