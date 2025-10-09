d841f17ff3dc3cf0ab4c64830e2e71d0dbb55fed
frank <frank@DESKTOP-EKQ6KKU>
2025-10-09 19:32:45 +0100

    implement migrations using my Windows computer

--- EasyGames/Migrations/20251009183134_track-backordered-items.Designer.cs
+++ EasyGames/Migrations/20251009183134_track-backordered-items.Designer.cs
@@ -4,6 +4,7 @@ using EasyGames.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EasyGames.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251009183134_track-backordered-items")]
+    partial class trackbackordereditems
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ EasyGames/Migrations/20251009183134_track-backordered-items.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EasyGames.Migrations
+{
+    /// <inheritdoc />
+    public partial class trackbackordereditems : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "QuantityBackordered",
+                table: "OrderItems",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "QuantityBackordered",
+                table: "OrderItems");
+        }
+    }
+}
